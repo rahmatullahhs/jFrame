ab55fc3d11a6a67a37fc9dcb9d476acad7e04ff3
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-05-22 17:03:15 +0600

    add

--- CreatReadDeletUpdate/src/creatreaddeletupdate/CreatReadDeletUpdate.java
+++ CreatReadDeletUpdate/src/creatreaddeletupdate/CreatReadDeletUpdate.java
@@ -15,8 +15,18 @@ public class CreatReadDeletUpdate {
 
     public static void main(String[] args) {
 
-        saveEmp("Rahmat", "Rahmat@mail.com", 9999999f);
-        System.out.println("After Save");
+//        saveEmp("rakib", "rakib@mail.com", 15000f);
+//        System.out.println("After Save");
+//        showAllEmp();
+//        System.out.println("--------------------------------");
+  // deletEmp(7);
+//        System.out.println("atfer delet");
+//      showAllEmp();
+//        System.out.println("--------------------------------");
+        updateEmp(1, "RahmatUllah", "Rahmat@mail.com", 35000f);
+//        System.out.println("after update");
+//        showAllEmp();
+//        System.out.println("--------------------------------");
 
     }
 
@@ -25,7 +35,7 @@ public class CreatReadDeletUpdate {
         sql = "insert into employee(name,email,salary)values(?,?,?)";
 
         try {
-            
+
             ps = db.getCon().prepareStatement(sql);
             ps.setString(1, name);
             ps.setString(2, email);
@@ -45,16 +55,57 @@ public class CreatReadDeletUpdate {
     }
 
     public static void showAllEmp() {
-        sql = "select*from employee";
+        sql = "select * from employee";
 
         try {
             ps = db.getCon().prepareStatement(sql);
 
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                    
-
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                float salary = rs.getFloat("salary");
+                System.out.println("ID: " + id + "Name: " + name + "Email: " + email + "Salary: " + salary);
             }
+            rs.close();
+            ps.close();
+            db.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CreatReadDeletUpdate.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+
+    public static void deletEmp(int id) {
+
+        sql = "delete from employee where id=?";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CreatReadDeletUpdate.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void updateEmp(int id, String name, String email, float salary) {
+        sql = "update employee set name=?, email=?, salary=? where id=?";
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setFloat(3, salary);
+            ps.setInt(4, id);
+
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
 
         } catch (SQLException ex) {
             Logger.getLogger(CreatReadDeletUpdate.class.getName()).log(Level.SEVERE, null, ex);
