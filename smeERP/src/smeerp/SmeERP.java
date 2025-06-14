27f2719f9b6dd75146a2cbee664c602fb9247655
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-06-14 18:50:18 +0600

    add

---
+++ smeERP/src/smeerp/SmeERP.java
@@ -0,0 +1,21 @@
+
+package smeerp;
+
+public class SmeERP {
+
+    public static void main(String[] args) {
+       
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
